82ff1f488d55830850f3d896218a438a45e08dad
Arsha8Anuga <lolbited@gmail.com>
2025-10-25 21:14:22 +0700

    simple todo list

--- C/excercise/simple_todo_list.c
+++ C/excercise/simple_todo_list.c
@@ -5,65 +5,123 @@
 
 #define NO_DESC
 
+typedef enum{
+  UPDATE_TITLE,
+  UPDATE_DESC,
+  UPDATE_STATUS,
+} OperationType;
 
+typedef enum{
+  NOT_STARTED_YET,
+  IN_PROGRESS,
+  DONE,
+  LATE
+} StatusAssign;
 
-typedef enum{ 
-    NOT_STARTED_YET,
-    IN_PROGRESS, 
-    DONE, 
-    LATE
-}StatusAssign;
-
-typedef struct{ 
-    unsigned int id;
-    char* title; 
-    char* description;
-    StatusAssign status;
-}TodoList;
-
+typedef struct{
+  unsigned int id;
+  char *title;
+  char *description;
+  StatusAssign status;
+} TodoList;
 
 static unsigned int id_cntr = 1;
 
+TodoList *createTodo(char*, char*, StatusAssign);
+int8_t updateTodo (TodoList*, OperationType,const void*);
+static inline int8_t updateTitle(TodoList*, char*);
+static inline int8_t updateDesc(TodoList*, char*);
+int8_t destroyTodo(TodoList*);
 
-TodoList* initTodo(char*, char*, StatusAssign);
+TodoList *createTodo(char* title, char* desc, StatusAssign stat){
 
-TodoList* initTodo(char* title, char* desc, StatusAssign stat){
+  if (!title) return NULL;
 
-    if(!title) return NULL;
+  TodoList *obj = (TodoList *)malloc(sizeof(TodoList));
+  if (!obj) return NULL;
+
+  int len_title = strlen(title);
+  int len_desc = (desc ? strlen(desc) : 0);
+
+  obj->title = (char *)malloc((len_title + 1) * sizeof(char));
+  if (!obj->title){
+    free(obj);
+    return NULL;
+  }
+  obj->description = (char *)malloc((len_desc + 1) * sizeof(char));
+  if (!obj->description){
     
-    TodoList* obj = (TodoList*)malloc(sizeof(TodoList));
-    if(!obj) return NULL;
-    
-    int len_title = strlen(title);
-    int len_desc = (desc ? strlen(desc) : 0);
+    free(obj->title);
+    free(obj);
+    return NULL;
+  }
 
-    obj->title = (char*)malloc((len_title + 1) * sizeof(char));
-    obj->description = (char*)malloc((len_desc + 1) * sizeof(char));
-    
-    obj->title = malloc(len_title + 1);
-    if (!obj->title) {
-        free(obj);
-        return NULL;
-    }
+  strcpy(obj->title, title);
+  if (desc){
+    strcpy(obj->description, desc);
+  }
+  else{
+    obj->description[0] = '\0';
+  }
 
-    obj->description = malloc(len_desc + 1);
-    if (!obj->description) {
-        free(obj->title);
-        free(obj);
-        return NULL;
-    }
-    
-    strcpy(obj->title, title);
-    if(desc){ 
-      strcpy(obj->description, desc);
-    }else{ 
-      obj->description[0] = '\0';
-    }
-    
+  obj->status = (stat >= NOT_STARTED_YET && stat <= LATE) ? stat : NOT_STARTED_YET;
+  obj->id = id_cntr++;
 
-    obj->status = (!stat ? NOT_STARTED_YET : stat);
-    obj->id = id_cntr++;
-
-    return obj;
+  return obj;
 }
 
+int8_t updateTodo(TodoList *obj, OperationType op, const void* val){
+
+  if(!obj || !val) return -1;
+
+  switch(op){
+    case UPDATE_TITLE:
+    case UPDATE_DESC: {
+      char* tmp = strdup((const char*)val);
+      if (!tmp) return -1; 
+      (op == UPDATE_TITLE) 
+        ? updateTitle(obj, tmp)
+        : updateDesc(obj, tmp);
+      break;
+    }
+    case UPDATE_STATUS: {
+      const StatusAssign* s = (const StatusAssign*)val;
+      obj->status = *s;
+      break;
+    }
+    default : return -1;
+  }
+
+  return 0;
+}
+
+static inline int8_t updateTitle(TodoList* obj, char* val){
+
+  if(!obj || !val) return -1;
+
+  free(obj->title);
+  obj->title = val;
+
+  return 0;
+}
+
+static inline int8_t updateDesc(TodoList* obj, char* val){
+
+  if(!obj || !val) return -1;
+
+  free(obj->description);
+  obj->description = val;
+
+  return 0;
+}
+
+int8_t destroyTodo(TodoList *obj){
+  if (!obj)
+    return -1;
+
+  free(obj->title);
+  free(obj->description);
+  free(obj);
+
+  return 0;
+}
